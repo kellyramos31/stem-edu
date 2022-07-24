c812af1c3e7d0859b904ca4919b1ebafefff6952
Kelly Ramos <ramoskel343@gmail.com>
2022-07-24 17:00:32 -0600

    adjust for deployment

--- client/src/components/AuthForm.js
+++ client/src/components/AuthForm.js
@@ -1,45 +1,32 @@
-import React from 'react'
+import React from "react";
 
-export default function AuthForm(props){
+export default function AuthForm(props) {
   const {
-    handleChange, 
-    handleSubmit, 
-    btnText, 
+    handleChange,
+    handleSubmit,
+    btnText,
     errMsg,
-    inputs: {
-      username, 
-      password,
-      adminCode
-    } 
-  } = props
-  
-  return (
+    inputs: { username, password, adminCode },
+  } = props;
 
+  return (
     <form className="auth-form" onSubmit={handleSubmit}>
-      <input 
-        type="text" 
-        value={username} 
-        name="username" 
-        onChange={handleChange} 
+      <input
+        type="text"
+        value={username}
+        name="username"
+        onChange={handleChange}
         placeholder="Username"
       />
-      <input 
-        type="text" 
-        value={password} 
-        name="password" 
-        onChange={handleChange} 
+      <input
+        type="text"
+        value={password}
+        name="password"
+        onChange={handleChange}
         placeholder="Password"
       />
-       <input 
-        type="text" 
-        value={adminCode} 
-        name="adminCode" 
-        onChange={handleChange} 
-        placeholder="Admin Code"
-      />
-      <button className="sign-up-btn">{ btnText }</button>
-      <p style={{color: "red"}}>{errMsg}</p>
+      <button className="sign-up-btn">{btnText}</button>
+      <p style={{ color: "red" }}>{errMsg}</p>
     </form>
-
-  )
+  );
 }
--- server.js
+++ server.js
@@ -1,48 +1,56 @@
-const express = require("express")
-const app = express()
-require("dotenv").config()
-const morgan = require("morgan")
-const mongoose = require("mongoose")
-const expressJwt = require("express-jwt")  //gatekeeper
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const expressJwt = require("express-jwt"); //gatekeeper
+const port = process.env.PORT || 5000;
+const path = require("path");
 
 //note: updates to syntax for line above would be {expressjwt} so it's a named import
-//& then would need to change line 26 to match casing -- expressjwt. 
+//& then would need to change line 26 to match casing -- expressjwt.
 // Also, on line 26, (already had it), have to add "algorithms: ['HS256'] in the object
 
 //************OTHER UPDATE: req.user is now req.auth************
 
-app.use(express.json())
-app.use(morgan("dev"))
+app.use(express.json());
+app.use(morgan("dev"));
 
 mongoose.connect(
-    "mongodb://localhost:27017/user-authentication3",
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: false
-    },
-    () => console.log("Connected to the DB")
-)
-//NOTE: update to syntax for mongoose connect -- can take out the 4 deprecations object (lines16-21)
+  process.env.MONGODB_URI,
+  {
+    useNewUrlParser: true,
+  },
+  () => console.log("Connected to the DB")
+);
 
-app.use("/auth", require("./routes/authRouter.js"))
-app.use("/api", expressJwt({secret: process.env.SECRET, algorithms: ['HS256']}))  //creates req.user(now req.auth) -- ALSO:  algorithms: for express-jwt v6.0.0 & higher: adding an algorithm parameter is now required in addition to the secret.
-//note: expressJwt line above checks to see if token has SECRET that 
+app.use(express.static(path.join(__dirname, "client", "build")));
+
+app.use("/auth", require("./routes/authRouter.js"));
+app.use(
+  "/api",
+  expressJwt({ secret: process.env.SECRET, algorithms: ["HS256"] })
+); //creates req.user(now req.auth) -- ALSO:  algorithms: for express-jwt v6.0.0 & higher: adding an algorithm parameter is now required in addition to the secret.
+//note: expressJwt line above checks to see if token has SECRET that
 //matches the one on the server => for any route beginning with /api;SO--the 3 routes below are protected
-app.use("/api/forumpost", require("./routes/forumPostRouter.js"))
-app.use("/api/comment", require("./routes/commentRouter.js"))
-app.use("/api/learngame", require("./routes/learnGameRouter.js"))
 
+app.use("/api/forumpost", require("./routes/forumPostRouter.js"));
+app.use("/api/comment", require("./routes/commentRouter.js"));
+app.use("/api/learngame", require("./routes/learnGameRouter.js"));
 
 app.use((err, req, res, next) => {
-    console.log(err)
-    if(err.name === "UnauthorizedError"){                  //this is for when no token available
-        res.status(err.status)
-    }
-    return res.send({ errMsg: err.message })
-})
+  console.log(err);
+  if (err.name === "UnauthorizedError") {
+    //this is for when no token available
+    res.status(err.status);
+  }
+  return res.send({ errMsg: err.message });
+});
 
-app.listen(8000, () => {
-    console.log("Server is running on local port 8000")
-})
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
+app.listen(port, () => {
+  console.log("Server is running on local port 5000");
+});
