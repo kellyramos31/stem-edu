cf0b07480a29c3fff5a11772948a54ead3074514
N R <ramoskel343@gmail.com>
2022-10-02 15:51:56 -0600

    profile pg title, font adjust

--- client/src/components/MyScores.js
+++ client/src/components/MyScores.js
@@ -1,22 +1,15 @@
-import React from "react"
-
+import React from "react";
 
 export default function MyScores(props) {
+  const { myScores } = props;
 
-const { myScores } = props
-
-return (
-
+  return (
     <div className="my-top-three-scores">
+      <h3 className="top-scores-title">Top 3 Game Scores</h3>
 
-            <h3 className="top-scores-title">My Top 3 Game Scores</h3>
-
-                {myScores.slice(0,3).map(score => (
-                   <h4 key={score._id}>✨{score.scoreTotal}{" "}points✨</h4>
-                ))}
-
+      {myScores.slice(0, 3).map((score) => (
+        <h4 key={score._id}>✨{score.scoreTotal} points✨</h4>
+      ))}
     </div>
-
-)
-
+  );
 }
