57dd39866334403efeb978e90301443876f80e39
N R <ramoskel343@gmail.com>
2022-10-02 11:15:52 -0600

    guest sign-in instructions

--- client/src/components/Auth.js
+++ client/src/components/Auth.js
@@ -1,67 +1,81 @@
-import React, { useState, useContext } from 'react'
-import AuthForm from './AuthForm.js'
-import { UserContext } from "../context/UserProvider.js"
+import React, { useState, useContext } from "react";
+import AuthForm from "./AuthForm.js";
+import { UserContext } from "../context/UserProvider.js";
 
-const initInputs = { username: "", password: ""}
+const initInputs = { username: "", password: "" };
 
-export default function Auth(){
-  const [inputs, setInputs] = useState(initInputs)
-  const [toggle, setToggle] = useState(false)
+export default function Auth() {
+  const [inputs, setInputs] = useState(initInputs);
+  const [toggle, setToggle] = useState(false);
 
-  const {signup, login, errMsg, resetAuthErr } = useContext(UserContext)
+  const { signup, login, errMsg, resetAuthErr } = useContext(UserContext);
 
-
-  function handleChange(e){
-    const {name, value} = e.target
-    setInputs(prevInputs => ({
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setInputs((prevInputs) => ({
       ...prevInputs,
-      [name]: value
-    }))
+      [name]: value,
+    }));
   }
 
-  function handleSignup(e){
-    e.preventDefault()
-    signup(inputs)
+  function handleSignup(e) {
+    e.preventDefault();
+    signup(inputs);
   }
 
-  function handleLogin(e){
-    e.preventDefault()
-    login(inputs)
+  function handleLogin(e) {
+    e.preventDefault();
+    login(inputs);
   }
 
-  function toggleForm(){
-    setToggle(prev => !prev)
-    resetAuthErr()
+  function toggleForm() {
+    setToggle((prev) => !prev);
+    resetAuthErr();
   }
 
   return (
     <div className="auth-container">
-      <img className="owl" src="./images/416387_bird_eyes_night_owl_vision_icon.png" alt="owl"/>
-      <h1 className="app-title">WiSE</h1>
-      <h5 className="app-sub-title">Women in STEM Education</h5>
-      { !toggle ?
+      <img
+        className="owl"
+        src="./images/416387_bird_eyes_night_owl_vision_icon.png"
+        alt="owl"
+      />
+      <h1 className="app-title">WISE</h1>
+      <h5 className="app-sub-title">Women In STEM Education</h5>
+      {!toggle ? (
         <>
-          <AuthForm 
+          <AuthForm
             handleChange={handleChange}
             handleSubmit={handleSignup}
             inputs={inputs}
             btnText="Sign up"
             errMsg={errMsg}
           />
-          <p onClick={toggleForm} className="member-or-not">Already a member?</p>
+          <p onClick={toggleForm} className="member-or-not">
+            Already a member?
+          </p>
+          <div className="sign-in-instructions">
+            <div className="title-instructions">SIGN-IN</div>
+            <div className="instruct">Click the "Already a member?" button</div>
+            <div className="instruct">and login with the following:</div>
+            <div className="login-instruct">username: guest</div>
+            <div className="login-instruct">password: guest</div>
+          </div>
         </>
-      :
+      ) : (
         <>
-          <AuthForm 
+          <AuthForm
             handleChange={handleChange}
             handleSubmit={handleLogin}
             inputs={inputs}
             btnText="Login"
             errMsg={errMsg}
           />
-          <p onClick={toggleForm} className="member-or-not">Not a member?</p>
+          <p onClick={toggleForm} className="member-or-not">
+            Not a member?
+          </p>
         </>
-      }
+      )}
     </div>
-  )
+  );
 }
